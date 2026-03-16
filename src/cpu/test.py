ea41c87357dbfca3dae321ed23c78e7fac261ba9
AmitavaDutta <guyfawkes.amitava20@gmail.com>
2026-03-17 00:04:41 +0530

    added a visualization test

---
+++ src/cpu/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset import generate_dataset
+
+def main():
+
+    N = 10
+    T = 100
+
+    data = generate_dataset(N, T)
+
+    print("Dataset shape (N,T):", data.shape)
+
+    # Compute correlation matrix
+    corr_matrix = np.corrcoef(data)
+
+    print("Correlation matrix shape (N,N):", corr_matrix.shape)
+
+    # visualize correlation matrix
+    plt.imshow(corr_matrix, cmap="coolwarm")
+    plt.colorbar(label="Correlation")
+    plt.title("Correlation Matrix")
+    plt.xlabel("Series Index")
+    plt.ylabel("Series Index")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
